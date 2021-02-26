c23ffdee5a9c888e0992bdf229eb1c93d1f239e6
nguyenhoangvudtm23 <68849645+nguyenhoangvudtm23@users.noreply.github.com>
2021-02-26 23:07:59 +0700

    Add files via upload

---
+++ Lab01/FirstDialog.java
@@ -0,0 +1,13 @@
+package Lab01;
+
+import javax.swing.JOptionPane;
+public class FirstDialog {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "Hello World");
+		System.exit(0);
+	}
+
+}
+
